a670fda1ba11c00248263bf1e9b6c02622f74bd5
steakinzer <clementjodin1u@gmail.com>
2023-04-24 17:03:48 +0200

    changing winning background color

--- script.js
+++ script.js
@@ -6,18 +6,16 @@ document.querySelector('.check').addEventListener('click', function () {
   let currentNumber = Number(document.querySelector('.guess').value);
   if (currentNumber < guestedNumber) {
     document.querySelector('.message').innerHTML = 'Plus grand';
-    // console.log(document.querySelector('.score').value);
     score--;
     document.querySelector('.score').textContent = score;
   } else if (currentNumber > guestedNumber) {
     document.querySelector('.message').innerHTML = 'Plus petit';
-    // document.querySelector('.score').textContent = "tu perd un point";
     score--;
     document.querySelector('.score').textContent = score;
   } else {
     document.querySelector('.message').innerHTML = 'Bravo tu as trouvé';
     document.querySelector('.check').style.display = 'none';
-    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').innerHTML = guestedNumber;
     if (Number(document.querySelector('.highscore').innerHTML) < score) {
       document.querySelector('.highscore').innerHTML = score;
